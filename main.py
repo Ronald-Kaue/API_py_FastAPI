959aada35a664f1c24381a1a9d72195af0e918cf
Ronald-Kaue <133374965+Ronald-Kaue@users.noreply.github.com>
2025-07-06 00:41:31 +0000

    Finalizando incrementação do PyJWT

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException
+from http import HTTPStatus
 from fastapi.security import OAuth2PasswordRequestForm
 import jwt
 from sqlalchemy.orm import Session
@@ -9,7 +10,7 @@ from schemas.comentario import ComentarioResponse, ComentarioCreate
 from schemas.token import Token
 from crud import *
 from database import engine, Base, get_db
-from security import create_access_token
+from security import create_access_token, current_user
 
 Base.metadata.create_all(bind=engine)
 
@@ -17,17 +18,21 @@ app = FastAPI()
 
 @app.post("/token", response_model=Token)
 def login(form_data: OAuth2PasswordRequestForm=Depends(), db: Session = Depends(get_db)):
-    user = Session.scalar(select(Usuario).where(Usuario.email == form_data.username))
-    if not user or not verify_password(form_data, Usuario.senha_hash):
+    user = db.scalar(select(Usuario).where(Usuario.email == form_data.username))
+    if not user or not verify_password(form_data.password, user.senha_hash):
         raise HTTPException(status_code=400, detail="Email ou Senha Inválidos")
-    access_token = create_access_token(data={'sub': Usuario.email})
+    access_token = create_access_token(data={'sub': user.email})
     return {'access_token': access_token, 'token_type': 'Bearer'}
+
+# Usuarios
 
 @app.post("/usuarios", response_model=UsuarioResponse)
 def criar_usuario_endpoint(usuario: UsuarioCreate, db: Session = Depends(get_db)):
     if get_usuario_por_email(db, usuario.email):
         raise HTTPException(status_code=400, detail="Email já cadastrado")
     return criar_usuario(db, usuario)
+
+# Mensagens
 
 @app.post("/usuarios/{usuario_id}/mensagens", response_model=MensagemResponse)
 def criar_mensagem_endpoint(usuario_id: int, mensagem: MensagemCreate, db: Session = Depends(get_db)):
@@ -38,48 +43,81 @@ def listar(db: Session = Depends(get_db)):
     return listar_mensagens(db)
 
 @app.get("/mensagens/{id}", response_model=MensagemResponse)
-def get_mensagem_endpoint(id: int, db: Session = Depends(get_db)):
-    msg = get_mensagem(db, id)
+def get_mensagem_endpoint(id: int, db: Session = Depends(get_db), current_user=Depends(current_user)):
+    if current_user.id != id:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Not enough permissions'
+        )
+    msg = get_mensagem(db, current_user.id)
     if not msg:
         raise HTTPException(status_code=404, detail="Mensagem não encontrada")
     return msg
 
 @app.put("/mensagens/{id}", response_model=MensagemResponse)
-def update_mensagem_endpoint(id: int, mensagem: MensagemCreate, db: Session = Depends(get_db)):
-    msg = atualizar_mensagem(db, id, mensagem.conteudo)
+def update_mensagem_endpoint(id: int, mensagem: MensagemCreate, db: Session = Depends(get_db), current_user=Depends(current_user)):
+    if current_user.id != id:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Not enough permissions'
+        )
+    msg = atualizar_mensagem(db, current_user.id, mensagem.conteudo)
     if not msg:
         raise HTTPException(status_code=404, detail="Mensagem não encontrada")
     return msg
 
 @app.delete("/mensagens/{id}")
-def deletar_endpoint(id: int, db: Session = Depends(get_db)):
-    msg = deletar_mensagem(db, id)
-    if not msg:
+def deletar_endpoint(id: int, db: Session = Depends(get_db), current_user=Depends(current_user)):
+    mensagem = get_mensagem(db, id)
+    if not mensagem:
         raise HTTPException(status_code=404, detail="Mensagem não encontrada")
+    if current_user.id != mensagem.usuario_id:
+        raise HTTPException(
+            status_code=HTTPStatus.FORBIDDEN,
+            detail='Not enough permissions'
+        )
+    deletar_mensagem(db, id)
     return {"INFO": "Mensagem deletada com sucesso"}
+
+# Comentarios
 
 @app.post("/mensagens/{mensagem_id}/comentarios", response_model=ComentarioResponse)
 def criar_comentario_endpoint(mensagem_id: int, comentario: ComentarioCreate, db: Session = Depends(get_db)):
     return criar_comentario(db, mensagem_id, comentario)
 
 @app.post("/usuario/{usuario_id}/mensagens/{mensagem_id}/comentarios", response_model=ComentarioResponse)
-def criar_comentario_with_user_endpoint(usuario_id:int, mensagem_id: int, comentario: ComentarioCreate, db: Session = Depends(get_db)):
-    return criar_comentario_com_user(db, mensagem_id, usuario_id, comentario)
+def criar_comentario_with_user_endpoint(usuario_id:int, mensagem_id: int, comentario: ComentarioCreate, db: Session = Depends(get_db), current_user=Depends(current_user)):
+    if current_user.id != id:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Not enough permissions'
+        )
+    return criar_comentario_com_user(db, mensagem_id, current_user.id, comentario)
 
 @app.get("/comentarios", response_model=list[ComentarioResponse])
 def listar(db: Session = Depends(get_db)):
     return listar_comentario(db)
 
 @app.put("/comentarios/{id}", response_model=ComentarioResponse)
-def update_comentario_endpoint(id: int, comentario: ComentarioCreate, db: Session = Depends(get_db)):
-    commentary = atualizar_comentario(db, id, comentario.conteudo)
+def update_comentario_endpoint(id: int, comentario: ComentarioCreate, db: Session = Depends(get_db), current_user=Depends(current_user)):
+    if current_user.id != id:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Not enough permissions'
+        )
+    commentary = atualizar_comentario(db, current_user.id, comentario.conteudo)
     if not commentary:
         raise HTTPException(status_code=404, detail="Comentário não encontrada")
     return commentary
 
 @app.delete("/comentarios/{id}")
-def deletar_endpoint(id: int, db: Session = Depends(get_db)):
-    commentary = deletar_comentario(db, id)
+def deletar_endpoint(id: int, db: Session = Depends(get_db), current_user=Depends(current_user)):
+    if current_user.id != id:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Not enough permissions'
+        )
+    commentary = deletar_comentario(db, current_user.id)
     if not commentary:
         raise HTTPException(status_code=404, detail="Comentário não encontrada")
     return {"INFO": "Comentário deletada com sucesso"}
--- security.py
+++ security.py
@@ -1,9 +1,19 @@
 from pwdlib import PasswordHash
-from jwt import encode
+from http import HTTPStatus
+from jwt import decode, encode
+from jwt.exceptions import PyJWTError
+from models.usuario import Usuario
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException
+from database import get_db
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 from zoneinfo import ZoneInfo
 from datetime import datetime, timedelta
+from models import usuario
 
 pwd_context = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 SECRET_KEY = 'Super-Secret-Key'
 ALGORITHM = 'HS256'
@@ -19,9 +29,30 @@ def create_access_token(data: dict):
     to_encode = data.copy()
 
     expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
-        minute=ACCESS_TOKEN_EXPIRE_MINUTES
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
     )
     to_encode.update({'exp': expire})
 
     encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def current_user(session: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=HTTPStatus.UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'}
+    )
+    try:
+        payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email= payload.get('sub')
+        if not email:
+            raise credentials_exception
+    except PyJWTError:
+        raise credentials_exception
+    
+    user = session.scalar(select(Usuario).where(Usuario.email == email))
+
+    if not user:
+        raise credentials_exception
+    
+    return user
