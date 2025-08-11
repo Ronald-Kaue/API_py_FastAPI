cb44a03916ad80a73575f2000b68723c2aac89b2
Ronald Kauê Dos Santos Bezerra <20221041110031@ifrn.local>
2025-08-11 09:56:28 -0300

    modificações

--- models/usuario.py
+++ models/usuario.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, func, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from zoneinfo import ZoneInfo
+from datetime import datetime
 from database import Base
 from pydantic import ConfigDict
 
@@ -11,6 +13,7 @@ class Usuario(Base):
     email = Column(String, unique=True, index=True)
     senha_hash = Column(String, nullable=False)
     role = Column(String, nullable=False, default = "user")
+    publicado_em = Column(DateTime, default=func.now())
     
     mensagens = relationship("Mensagem", back_populates="autor")
     comentario = relationship("Comentario", back_populates="autor")
